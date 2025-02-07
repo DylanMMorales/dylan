0d163faf1bd4957bc70f1deeab11c9030a52653a
DylanMMorales <157064138+DylanMMorales@users.noreply.github.com>
2025-02-07 10:15:43 -0500

    Name and Birthday

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-#!/usr/bin/env node
-console.log("dylan");
+#!/usr/bin/env bash
 
+const myName = "Dylan";
+let myDay = "My birthday day";
+myDay = "Friday";
+const message = `Howdy! I'm ${myName} and my special day this year is ${myDay}!`;
+console.log(message);
